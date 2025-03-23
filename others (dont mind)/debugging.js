6584b99a54fe8a6a574c64e947da3169bd34a2f5
kat-mg <kat.ann.mislang@gmail.com>
2025-03-24 01:53:19 +0800

    meshData is now input from file

--- others (dont mind)/debugging.js
+++ others (dont mind)/debugging.js
@@ -1,18 +1,18 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /* Functions */
-const toRadians = (degrees) => degrees * Math.PI / 180
+const toRadians = (degrees) => degrees * Math.PI / 180;
 
 const latlongToCartesian = (latitude, longitude, radius) => {
-    const lat = toRadians(latitude)
-    const long = toRadians(longitude)
+    const lat = toRadians(latitude);
+    const long = toRadians(longitude);
 
-    const x = radius * Math.cos(lat) * Math.cos(long)
-    const y = radius * Math.cos(lat) * Math.sin(long)
-    const z = radius * Math.sin(lat)
+    const x = radius * Math.cos(lat) * Math.cos(long);
+    const y = radius * Math.cos(lat) * Math.sin(long);
+    const z = radius * Math.sin(lat);
 
-    return [x, y, z]
+    return [x, y, z];
 }
 
 function createSphericalCurve(pointA, pointB, radius, segments = 100) {
@@ -33,142 +33,169 @@ function createSphericalCurve(pointA, pointB, radius, segments = 100) {
 }
 
 /* Data */
-const meshData = {
-    vertices: [
-        [90, 0], [1, -180], [0, -90], [0, 0], [-1, 90], [-90, 0]
-    ],
-    faces: [
-        [0, 1, 2], [0, 3, 4], [0, 4, 1],
-        [1, 5, 2], [2, 5, 3], [3, 5, 4], [4, 5, 1]
-    ]
-};
+THREE.Cache.enabled = true;
+const fileLoader = new THREE.FileLoader();
 
-/* Scene */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+async function loadFile() {
+    return new Promise((resolve, reject) => {
+        let meshData = { vertices: [], faces: [] };
 
-// Scene
-const scene = new THREE.Scene();
-
-// Sphere Object
-const geometrySphere = new THREE.SphereGeometry(1, 32, 32);
-const materialSphere = new THREE.MeshBasicMaterial({ color: 0x0b5208, transparent: true, opacity: 0.3, wireframe: true });
-const meshSphere = new THREE.Mesh(geometrySphere, materialSphere);
-scene.add(meshSphere);
-
-// Particles Object
-const particleGeometry = new THREE.BufferGeometry();
-const particleCount = meshData.vertices.length;
-const particlePositions = new Float32Array(particleCount * 3);
-
-for (let i = 0; i < particleCount; i++) {
-    console.log(i, meshData.vertices[i]);                       // For debugging purposes
-    const lat = meshData.vertices[i][0];
-    const long = meshData.vertices[i][1];
-    const [x, y, z] = latlongToCartesian(lat, long, 1);
-    meshData.vertices[i] = [x, y, z];
-    console.log(meshData.vertices[i]);                       // For debugging purposes
+        fileLoader.load('cubic.sph', 
+            function (data) {
+                console.log(data);                  // For debugging purposes
+                const lines = data.split('\n');
+                const line1 = lines[1].split(' ');
+                const noVertices = parseInt(line1[0]);
+                const noFaces = parseInt(line1[1]);         // For debugging purposes
+                console.log("Verts:", noVertices, "Faces:", noFaces);         // For debugging purposes
+                for (let i = 2; i < lines.length; i++) {
+                    console.log("i", i, "Line:", line);         // For debugging purposes
+                    const line = lines[i].split(' ');
+                    if (i - 2 < noVertices) {
+                        meshData.vertices.push([parseFloat(line[0]), parseFloat(line[1])]);
+                    }
+                    else {
+                        const noSides = parseInt(line[0]);
+                        let thisFace = [];
+                        for (let j = 0; j < noSides; j++) {
+                            thisFace.push(parseInt(line[1 + j]));
+                        }
+                        meshData.faces.push(thisFace);
+                    }
+                }
+                resolve(meshData);
+            },
+            undefined,
+            function (error) {
+                reject(error);
+            }
+        );
+    });
 }
 
-for (let i = 0; i < particleCount; i++) {
-    console.log(meshData.vertices[i]);
-    particlePositions[i * 3] = meshData.vertices[i][0];
-    particlePositions[i * 3 + 1] = meshData.vertices[i][1];
-    particlePositions[i * 3 + 2] = meshData.vertices[i][2];
-}
+async function init() {
+    const meshData = await loadFile();
+    /* Scene */
+    // Canvas
+    const canvas = document.querySelector('canvas.webgl');
 
-particleGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+    // Scene
+    const scene = new THREE.Scene();
 
-const particleMaterial = new THREE.PointsMaterial();
-particleMaterial.size = 0.03;
-particleMaterial.sizeAttenuation = true;
-particleMaterial.wireframe = true;
-const particles = new THREE.Points(particleGeometry, particleMaterial);
-scene.add(particles);
+    // Sphere Object
+    const geometrySphere = new THREE.SphereGeometry(1, 32, 32);
+    const materialSphere = new THREE.MeshBasicMaterial({ color: 0x0b5208, transparent: true, opacity: 0.3, wireframe: true });
+    const meshSphere = new THREE.Mesh(geometrySphere, materialSphere);
+    scene.add(meshSphere);
 
-// Lines TODO
-let noLines = 0;
-const lines = [];
-for (let i = 0; i < meshData.faces.length; i++) {
-    console.log("Polygon:", i);
-    for (let j = 0; j < meshData.faces[i].length - 1; j++) {
-        const currVertex = meshData.vertices[meshData.faces[i][j]];
-        let connectTo = meshData.vertices[meshData.faces[i][j + 1]];
+    // Particles Object
+    const particleGeometry = new THREE.BufferGeometry();
+    const particleCount = meshData.vertices.length;
+    const particlePositions = new Float32Array(particleCount * 3);
 
-        console.log(meshData.faces[i][j], currVertex, "is gonna be connected to", meshData.faces[i][j + 1], connectTo);         // For debugging purposes
-
-        const line = createSphericalCurve(currVertex, connectTo, 1);
-        lines.push(line);
-        noLines++;
+    for (let i = 0; i < particleCount; i++) {
+        console.log("Start", meshData.vertices[i]);
+        const lat = meshData.vertices[i][0];
+        const long = meshData.vertices[i][1];
+        const [x, y, z] = latlongToCartesian(lat, long, 1);
+        meshData.vertices[i] = [x, y, z];
+        console.log(meshData.vertices[i]);
+        console.log("End", meshData.vertices[i]);
     }
-}
-console.log(noLines);           // For debugging purposes
 
-let currentLineIndex = 0;
-
-function addNextLine() {
-    if (currentLineIndex < lines.length) {
-        scene.add(lines[currentLineIndex]);
-        console.log("Line added:", lines[currentLineIndex]);           // For debugging purposes
-        currentLineIndex++;
-        setTimeout(addNextLine, 2000); // Add the next line after 2 seconds
+    for (let i = 0; i < particleCount; i++) {
+        console.log(meshData.vertices[i]);
+        particlePositions[i * 3] = meshData.vertices[i][0];
+        particlePositions[i * 3 + 1] = meshData.vertices[i][1];
+        particlePositions[i * 3 + 2] = meshData.vertices[i][2];
     }
+
+    particleGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+
+    const particleMaterial = new THREE.PointsMaterial();
+    particleMaterial.size = 0.03;
+    particleMaterial.sizeAttenuation = true;
+    particleMaterial.wireframe = true;
+    const particles = new THREE.Points(particleGeometry, particleMaterial);
+    scene.add(particles);
+
+    // Lines TODO
+    for (let i = 0; i < meshData.faces.length; i++) {
+        console.log("Polygon:", i);         // For debugging purposes
+        for (let j = 0; j < meshData.faces[i].length; j++) {
+            let currVertex, connectTo;
+            if (j === meshData.faces[i].length - 1) {
+                currVertex = meshData.vertices[meshData.faces[i][j]];
+                connectTo = meshData.vertices[meshData.faces[i][0]];
+            }
+            else {
+                currVertex = meshData.vertices[meshData.faces[i][j]];
+                connectTo = meshData.vertices[meshData.faces[i][j + 1]];
+            }
+
+            console.log(meshData.faces[i][j], currVertex, "is gonna be connected to", meshData.faces[i][j + 1], connectTo);         // For debugging purposes
+
+            const line = createSphericalCurve(currVertex, connectTo, 1);
+            scene.add(line);
+        }
+    }
+
+    /* Sizes */
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+
+    window.addEventListener('resize', () =>
+    {
+        // Update sizes
+        sizes.width = window.innerWidth;
+        sizes.height = window.innerHeight;
+
+        // Update camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
+
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    /* Camera */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    /* Renderer */
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    /* Animate */
+    const clock = new THREE.Clock();
+
+    const tick = () =>
+    {
+        const elapsedTime = clock.getElapsedTime();
+
+        // Update controls
+        controls.update();
+
+        // Render
+        renderer.render(scene, camera);
+
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick);
+    }
+
+    tick();
 }
 
-/* Sizes */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/* Camera */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/* Renderer */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/* Animate */
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
-addNextLine(); // Start adding lines with a delay
+init();
